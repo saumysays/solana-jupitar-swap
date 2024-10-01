47d3cdbf4fd567586dac3263d2a5444a017aba4c
Saumya Pathak <95231428+saumysays@users.noreply.github.com>
2024-10-01 20:09:49 +0530

    Delete solana-jupitar-swap directory

--- solana-jupitar-swap/src/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  CLUSTER: process.env.CLUSTER || 'devnet',
-  INPUT_MINT: process.env.INPUT_MINT,
-  OUTPUT_MINT: process.env.OUTPUT_MINT,
-  AMOUNT_TO_SWAP: process.env.AMOUNT_TO_SWAP,
-};
